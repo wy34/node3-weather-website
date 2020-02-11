43e1853f1f1b031e01d37243ea48612c49a600a9
William Yeung <williamyeung1141998@gmail.com>
2020-02-10 21:46:25 -0500

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log("Client side javascript is running")
-
-fetch("http://puzzle.mead.io/puzzle")
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data)
-    })
-    
-
-
-
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 const messageOne = document.querySelector("#message-1")
