cec39caf56fa34bd1c8d58587e699ab9f792825e
William Yeung <williamyeung1141998@gmail.com>
2020-02-10 21:43:38 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require("request")
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/9402e031ac601cb08f2b33fcb50ae8df/${latitude},${longitude}`
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback("Unable to connect to weather services", undefined)
+        } else if (body.error) {
+            callback("Unable to find location", undefined)
+        } else {
+            const currentlyData = body.currently
+            const dailyData = body.daily.data[0].summary;   
+            callback(undefined, `${dailyData} It is currently ${currentlyData.temperature} degrees out. There is a ${currentlyData.precipProbability}% chance of rain.`)
+        }
+    })
+}
+
+module.exports = forecast;
